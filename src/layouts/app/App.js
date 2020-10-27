300a09e4305013eba9ba9cde184d3efeb6925264
Volodymyr Hryniuk <ghyniuk@gmail.com>
2020-10-27 10:51:15 +0100

    Configuration

---
+++ src/layouts/app/App.js
@@ -0,0 +1,13 @@
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+         <h1>Ты всех развалишь)) Motivation 1.0</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
